c464d7956ce5124b64380a38aa3b7d47269d48ab
alvaro <almamani@agetic.gob.bo>
2021-03-11 15:46:08 -0400

    integracion de contador totp

--- src/app.js
+++ src/app.js
@@ -6,20 +6,44 @@ import './mycss2.css';
 let appLib = new AppLib();
 
 let myapp = {
-    myfunction : function () { document.getElementById('info').innerHTML = NL_NAME + " is running on port " +
-                    NL_PORT + " inside " + NL_OS + "<br/><br/>" + "<span>v" + NL_VERSION + "</span>"; }
+    getRemaining: () => {
+        const remaining = authenticator.timeRemaining();
+        document.getElementById('count').innerHTML =  `Actualizando en ${remaining} segundos`;
+        document.getElementById('myBar').style.width = `${remaining * 100 / 30}%`;
+    },
+    setTotp: (secret) => {
+        const token = authenticator.generate(secret);
+        document.getElementById('otp').innerHTML = token;
+    },
+    setDate: () => {
+        document.getElementById('date').innerHTML = new Date();
+    },
+    generateSecret: () => {
+        const secret = authenticator.generateSecret();
+        document.getElementById('secret').innerHTML = secret;
+        return secret;
+    }
 };
     
 
 Neutralino.init({
     load: function() {
-        //alert(authenticator.generateSecret());
+        // set date
+        myapp.setDate();
+        setInterval(() => {
+          myapp.setDate();
+        }, 1000);
+
+        // button generate secret
         document.getElementById('generate-secret').onclick = function () {
-            const secret = authenticator.generateSecret();
-            document.getElementById('secret').innerHTML = secret;
+            const secret = myapp.generateSecret();
+            myapp.setTotp(secret);
+            myapp.getRemaining();
+            setInterval(() => {
+                myapp.getRemaining();
+                myapp.setTotp(secret);
+            }, 1000);
         }
-        //myapp.myfunction();
-        //appLib.showSettings();
     },
     pingSuccessCallback : function() {
 
